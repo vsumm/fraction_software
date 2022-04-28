7f45f1a01e35c5cc41d6530aac1da413ffc16451
vsumm <sumis2140@gmail.com>
2022-04-28 16:12:40 +0530

    Updates

---
+++ precache-manifest.3ef0e744a430fdc780c7a88284b422f1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b47bbc32b07749347f6",
+    "url": "/fraction_software/static/css/main.2f935654.chunk.css"
+  },
+  {
+    "revision": "9b47bbc32b07749347f6",
+    "url": "/fraction_software/static/js/main.9b47bbc3.chunk.js"
+  },
+  {
+    "revision": "c75c93e26160fab0ca55",
+    "url": "/fraction_software/static/css/1.2b9fae96.chunk.css"
+  },
+  {
+    "revision": "c75c93e26160fab0ca55",
+    "url": "/fraction_software/static/js/1.c75c93e2.chunk.js"
+  },
+  {
+    "revision": "6b9f817c3db115a5df52",
+    "url": "/fraction_software/static/js/2.6b9f817c.chunk.js"
+  },
+  {
+    "revision": "9dad515986479f985318",
+    "url": "/fraction_software/static/js/3.9dad5159.chunk.js"
+  },
+  {
+    "revision": "472effa229dcd6016ce8",
+    "url": "/fraction_software/static/js/4.472effa2.chunk.js"
+  },
+  {
+    "revision": "d01246ace9a0c2693460",
+    "url": "/fraction_software/static/js/runtime~main.d01246ac.js"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/fraction_software/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/fraction_software/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "241903cf740e11c47a5fd0b9133159cc",
+    "url": "/fraction_software/static/media/logo.241903cf.png"
+  },
+  {
+    "revision": "22c2d0c8a862dfaa43a271092986386c",
+    "url": "/fraction_software/static/media/profile.22c2d0c8.jpg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/fraction_software/static/media/svg-3.397c455a.svg"
+  },
+  {
+    "revision": "05620aaaab469ff11839669e97590168",
+    "url": "/fraction_software/index.html"
+  }
+];
